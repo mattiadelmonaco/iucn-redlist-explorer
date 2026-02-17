720d2bf7b09ab3b4a8ac61f5ce4f427bb08dea4f
mattiadelmonaco <mattia.delmonaco@gmail.com>
2026-02-17 16:23:25 +0100

    feat: add favorites migration and model

---
+++ app/Models/Favorite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+    protected $fillable = [
+        'sis_taxon_id',
+        'scientific_name',
+        'common_names',
+    ];
+
+    protected $casts = [
+        'common_names' => 'array',
+    ];
+}
---
+++ database/migrations/2026_02_17_151714_create_favorites_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('sis_taxon_id')->unique();
+            $table->string('scientific_name');
+            $table->json('common_names')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
