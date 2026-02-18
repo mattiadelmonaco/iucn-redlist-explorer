dc80f268d4b1078ee28f18b26fb4d6ecc76cf055
mattiadelmonaco <mattia.delmonaco@gmail.com>
2026-02-18 13:26:01 +0100

    feat: add getSystems and getCountries methods

--- app/Services/IucnApiService.php
+++ app/Services/IucnApiService.php
@@ -3,15 +3,36 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Cache;
 
 class IucnApiService
 {
     private string $baseUrl = 'https://api.iucnredlist.org/api/v4';
-    private string $apiKey;
+    private string $apiKey; // token autenticazione API IUCN
 
     public function __construct()
     {
         $this->apiKey = config('services.iucn.key');
+    }
+
+    /**
+     * recupera lista dei sistemi (Terrestre, Acqua dolce, Marino)
+     */
+    public function getSystems(): array
+    {
+        $response = Http::withToken($this->apiKey)
+            ->get("{$this->baseUrl}/systems/");
+
+        return $response->json();
+    }
+
+    /**
+     * recupera lista nazioni
+     */
+    public function getCountries(): array
+    {
+        $response = Http::withToken($this->apiKey)
+            ->get("{$this->baseUrl}/country/list");
+
+        return $response->json();
     }
 }
